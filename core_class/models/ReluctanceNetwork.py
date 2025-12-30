6e845f4bdc4e029b13ea5c4ff94340d3de2f8ee5
iam-vuvandat <iam.vuvandat@gmail.com>
2025-12-30 23:17:23 +0700

    #

--- core_class/models/ReluctanceNetwork.py
+++ core_class/models/ReluctanceNetwork.py
@@ -63,7 +63,7 @@ class ReluctanceNetwork:
 
     def solve_magnetic_equation(self,
                                 method = "conjugate_gradient",
-                                max_iteration = 11,
+                                max_iteration = 20,
                                 max_relative_residual = 1 * 1e-4,
                                 adaptive_damping_factor = (0.11,0.11),
                                 load_step = 1,
