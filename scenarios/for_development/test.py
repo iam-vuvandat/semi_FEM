dbc50e71603ae73ae77f2dea4d27e8195e39c77c
iam-vuvandat <iam.vuvandat@gmail.com>
2026-04-16 07:29:38 +0700

    Dọn dẹp thư mục

--- scenarios/for_development/test.py
+++ scenarios/for_development/test.py
@@ -6,7 +6,7 @@ from src.core.storage.core import motor_io
 from src.core.motor_type.models.axial_flux_motor_type_1 import AxialFluxMotorType1
 
 # Option
-reload_motor = True
+reload_motor = False
 file_name = "motor_test"
 export_maxwell = True
 solve_semiFEM = True
